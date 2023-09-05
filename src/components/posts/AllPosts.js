48c0556a82e9f4072acf355785faf69111a12101
Nathan Chevalier <cerberusgfx@gmail.com>
2023-09-05 14:55:18 -0500

    Post filters working correctly

--- src/components/posts/AllPosts.js
+++ src/components/posts/AllPosts.js
@@ -1,15 +1,33 @@
+import "./posts.css";
 import { useState, useEffect } from "react";
 import { getAllPosts } from "../../services/postService";
-import "./posts.css";
+import { getAllTopics } from "../../services/topicService";
 
 export const AllPosts = () => {
   const [posts, setPosts] = useState([]);
+  const [topics, setTopics] = useState([]);
+  const [filteredPosts, setFilteredPosts] = useState([]);
+  const [showTopic, setShowTopic] = useState(0);
 
   useEffect(() => {
     getAllPosts().then((postArray) => {
       setPosts(postArray);
     });
+    getAllTopics().then((topicArray) => {
+      setTopics(topicArray);
+    });
   }, []);
+
+  useEffect(() => {
+    if (showTopic > 0) {
+      const postFilter = posts.filter(
+        (post) => post.topicId === parseInt(showTopic)
+      );
+      setFilteredPosts(postFilter);
+    } else {
+      setFilteredPosts(posts);
+    }
+  }, [posts, showTopic]);
 
   return (
     <>
@@ -18,10 +36,22 @@ export const AllPosts = () => {
           <div>
             <h2>All Posts</h2>
           </div>
-          <div></div>
+          <div>
+            <select
+              className="topic-filter"
+              onChange={(event) => {
+                setShowTopic(event.target.value);
+              }}
+            >
+              <option value={0}> Displaying All Topics</option>
+              {topics.map((topic) => {
+                return <option value={topic.id}>{topic.name}</option>;
+              })}
+            </select>
+          </div>
         </div>
         <ul>
-          {posts.map((post) => {
+          {filteredPosts.map((post) => {
             return (
               <li key={post.id} className="post-list">
                 <div className="post-title">{post.title}</div>
